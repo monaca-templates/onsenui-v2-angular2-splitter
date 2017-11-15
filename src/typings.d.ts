6e53f092b6cee45ed7515e9560db2127b055275b
Naoki Matagawa <naoki.m@asial.co.jp>
2017-11-15 20:25:01 +0900

    Remove outdated aliases.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -31,24 +31,3 @@ import * as _ from 'lodash'
 
 declare var require: any;
 declare var process: any;
-
-// Alias legacy import/exports pre rc
-declare module "angular2/core" {
-  export * from '@angular/core';
-}
-
-declare module "angular2/http" {
-  export * from '@angular/http';
-}
-
-declare module 'angular2/platform/browser' {
-  export * from '@angular/platform-browser'
-}
-
-declare module 'angular2/testing' {
-  export * from '@angular/core/testing';
-}
-
-declare module 'angular2/http/testing' {
-  export * from '@angular/http/testing';
-}
